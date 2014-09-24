7747961185d194f07da5721c4093dc54786f40e0
Edward Wenger <ewenger@intven.com>
2014-09-24 04:08:28 +0100

    Remake histograms with more Sierra Leone districts

--- plot_ebola_cases.py
+++ plot_ebola_cases.py
@@ -42,7 +42,7 @@ week_number_of_first_of_month=[d/7.0 for d in first_day_of_months]
 def plot_site(ax,country,district,color,labels=['left']):
     plt.bar(week_numbers, cases[country][district], width=0.8, color=color, alpha=0.5)
     adjust_spines(ax,labels)
-    plt.xlim([0.5,37.5]) # weeks to mid-September
+    plt.xlim([0.5,38.5]) # weeks to mid-September
     #plt.ylim([0,max(50,max(cases[country][district])+1)])
     #plt.yticks(range(0,150,50))
     plt.yticks([min(cases[country][district]), max(cases[country][district])])
@@ -54,21 +54,23 @@ def plot_site(ax,country,district,color,labels=['left']):
 
 cases = get_ebola_counts_by_country(['Guinee','Sierra_Leone','Liberia'])
 
-sites=[ ('Liberia','Lofa','salmon'),
-        ('Liberia','Montserrado','darkred'),
-        ('Liberia','Margibi','r'),
+sites=[ #('Liberia','Lofa','salmon'),
+        #('Liberia','Montserrado','darkred'),
+        #('Liberia','Margibi','r'),
         #('Liberia','Nimba','r'),
         #('Liberia','Bong','r'),        
         ('Sierra_Leone','Kailahun','darkseagreen'),
         ('Sierra_Leone','Kenema','g'),
-        #('Sierra_Leone','Kono','g'),
-        #('Sierra_Leone','Bo','g'),        
+        ('Sierra_Leone','Kono','lightgreen'),
+        ('Sierra_Leone','Bo','forestgreen'),
+        ('Sierra_Leone','Bombali','g'),                
         ('Sierra_Leone','Western (urban)','olive'),
-        #('Sierra_Leone','Western (rural)','olive'),        
-        ('Sierra_Leone','Port Loko','darkolivegreen'),
-        ('Guinee','Conakry','darkblue'),
-        ('Guinee','Macenta','b'),
-        ('Guinee','Gueckedou','dodgerblue',['bottom','left']) ]
+        ('Sierra_Leone','Western (rural)','olivedrab'),        
+        ('Sierra_Leone','Port Loko','darkolivegreen',['bottom','left'])
+        #('Guinee','Conakry','darkblue'),
+        #('Guinee','Macenta','b'),
+        #('Guinee','Gueckedou','dodgerblue',['bottom','left'])
+        ]
 
 plt.figure('Ebola cases', figsize=(8,8), facecolor='w')
 for i,site in enumerate(sites):
