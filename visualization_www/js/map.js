1e4f768ae31aea066d213d0e782cb665c473014e
Amelia B-V <bertozzivill@gmail.com>
2014-10-30 18:07:59 -0700

    Changed color scheme and subnational color scales; implemented case message on hover event.

--- visualization_www/js/map.js
+++ visualization_www/js/map.js
@@ -34,35 +34,51 @@
 //////////////////
 ///PLOTTING
 //////////////////
+
 	//assign names to national paths
-		var national_settings = {0: {'iso3':'GIN',
-								  'color': 'green'},
-							  1: {'iso3':'LBR',
-								  'color': 'blue'},
-							  2: {'iso3':'SLE',
-								  'color': 'red'}}
+		var national_settings = [{'iso3':'GIN',
+								  'color': 'purple'},
+							    {'iso3':'LBR',
+								  'color': 'purple'},
+							    {'iso3':'SLE',
+								  'color': 'purple'}]
 
 
-	//Function to get all the values out of key-value pairs of an object
-	var getValues = function(obj){
-		var values = []
-		for (var key in obj){
-			values.push(obj[key])
+	// aggregate subnational and national data to get proper colorscales on the district and national level 
+
+		//combine all the subnational data; find the biggest value
+		var all_data = []
+		var summed_vals = {}
+		for (i=0; i<national_settings.length; i++) {
+			var nat_name = national_settings[i]['iso3']
+			var nat_data = all_case_data[nat_name]['Cumulative']
+			var nat_array = d3.keys(nat_data).map(function(d) { return nat_data[d] })
+			all_data.push.apply(all_data, nat_array)
+			summed_vals[nat_name] = d3.sum(nat_array)
 		}
-		return values
-	}
+
+		//set minima and maxima for district-level colorscale
+		var district_min = d3.min(all_data)
+		var district_max = d3.max(all_data)
+
+		//set minima and maxima for national-level colorscale 
+		var summed_array = d3.keys(summed_vals).map(function(d){return summed_vals[d]})
+		var national_min = d3.min(summed_array)
+		var national_max = d3.max(summed_array)
 
 
 	//function for subnat plotting on national click
 	var showSubnats = function(d){
-		//get rid of the current subnats ones
-			map_g.selectAll('.subnational').remove()
-			//$('.subnational').fadeOut(400)
 
 		//get properties of national value, load shapefile
 			var nat_name = $(this).attr('id')
 			var nat_color = $(this).attr('fill')
 			var subnat_shape = topojson.feature(subnats, subnats.objects[nat_name + '_subnat']).features
+
+		//get rid of the current subnats 
+			map_g.selectAll('.subnational').remove()
+			//$('.subnational').fadeOut(400)
+
 
 		//load file that maps elements of the ordered list of subnationals to the subnat names
 			var name_map = subnat_name_mapping[nat_name]
@@ -72,17 +88,15 @@
 			var cumulative_cases = national_cases['Cumulative']
 
 		// get min and max, and set up a color scale
-			var case_values = getValues(cumulative_cases)
-			var min = d3.min(case_values)
-			var max = d3.max(case_values)
-			var color_scale = d3.scale.linear().range(['white', nat_color]).domain([min, max])
+			var case_values = d3.keys(cumulative_cases).map(function(d) {return cumulative_cases[d]})
+			var color_scale = d3.scale.linear().range(['white', nat_color]).domain([district_min, district_max])
 
 		//plot subnats
-			var subnat_paths = map_g.selectAll('.' + nat_name)
+			var subnat_paths = map_g.selectAll('.subnational')
 					.data(subnat_shape)
 					.enter()
 					.append('path')
-					.attr('class', 'subnational ' + nat_name)
+					.attr('class', 'subnational' )
 					.attr('id', function(d,i){return name_map[i]})
 					.attr('parent_id', nat_name)
 					.attr('fill', nat_color)
@@ -96,15 +110,35 @@
 				var color = color_scale(value)
 				return color
 			})
+
+		//mouseover text: list name of province and number of cases so far
+			$('#map-g .subnational').poshytip({
+			alignTo: 'cursor', // Align to cursor
+			followCursor: true, // follow cursor when it moves
+			showTimeout: 0, // No fade in
+			hideTimeout: 0,  // No fade out
+			alignX: 'center', // X alignment
+			alignY: 'inner-bottom', // Y alignment
+			className: 'tip-twitter', // Class for styling
+			offsetY: 10, // Offset vertically
+			slide: false, // No slide animation
+			content: function(d){
+				var obj = this.__data__ // Data associated with element
+				var name = $(this).attr('id') // Name from properties
+				var nat_name = $(this).attr('parent_id')
+				var cases = all_case_data[nat_name]['Cumulative'][name] // iso3
+				return name + ' : ' + cases + ' Cases' // String to return
+			}
+			})
 	
 	}
 
 	//define national data
-	var nat_data = topojson.feature(national, national.objects.layer).features
+	var nat_shape_data = topojson.feature(national, national.objects.layer).features
 
 	//plot outlines of countries
 		var national_paths = map_g.selectAll('.national')
-			.data(nat_data)
+			.data(nat_shape_data)
 			.enter()
 			.append('path')
 			.attr('class', 'national')
@@ -117,28 +151,18 @@
 		  .on('click', showSubnats)
 
 
-		// //mouseover text: list name of province and number of cases so far
-		// $('#map-g path').poshytip({
-		// 	alignTo: 'cursor', // Align to cursor
-		// 	followCursor: true, // follow cursor when it moves
-		// 	showTimeout: 0, // No fade in
-		// 	hideTimeout: 0,  // No fade out
-		// 	alignX: 'center', // X alignment
-		// 	alignY: 'inner-bottom', // Y alignment
-		// 	className: 'tip-twitter', // Class for styling
-		// 	offsetY: 10, // Offset vertically
-		// 	slide: false, // No slide animation
-		// 	content: function(d){
-		// 		var obj = this.__data__ // Data associated with element
-		// 		console.log(obj)
-		// 		var name = $(this).attr('id') // Name from properties
-		// 		console.log(name)
-		// 		var nat_name = $(this).attr('parent_id')
-		// 		console.log(nat_name)
-		// 		var cases = all_case_data[nat_name]['Cumulative'][name] // iso3
-		// 		//mean = data[iso3] == undefined ? '' : data[iso3].mean // Value
-		// 		return name + ' : ' + cases + ' Cases' // String to return
-		// 	}
-		// })
+		//make national fill dependent on cumulative cases nationally
+		national_paths.attr('fill', function(d,i){
+			var nat_name = $(this).attr('id')
+			var base_color = national_settings[i]['color']
+			var nat_cases = summed_vals[nat_name]
+			var nat_color_scale = d3.scale.linear().range(['white', base_color]).domain([0, national_max])
+			var nat_color = nat_color_scale(nat_cases)
+			return nat_color
+		})
+
+
+
+
 
 
