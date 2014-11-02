23ad559439e00e4e7799094eca222b156b000b6d
Edward Wenger <ewenger@intven.com>
2014-11-01 22:28:39 -0700

    First pages commit

---
+++ js/map.js
@@ -0,0 +1,184 @@
+//Subnational-level map of Ebola
+
+
+//////////////////
+///SETUP
+//////////////////
+
+	//Projections, locate things on page
+		var settings = {width: document.getElementById('map-container').clientWidth,
+						height: 800,
+						padY: 20,
+						padX: -180}
+
+		// Set map projection
+			 var center = d3.geo.centroid(topojson.feature(national, national.objects.layer))
+			 var map_scale  = 3800;
+			 var projection = d3.geo.equirectangular().scale(map_scale).center(center)
+					  //.translate([settings.width/2, settings.height/4]);
+					  //.translate([settings.width/2, settings.height/4]);
+
+				// Set path generator -- how coordinates translate into a path element
+				var path = d3.geo.path().projection(projection)
+
+		//generate main SVG, smaller G which will contain the actual map, and title G
+			var full_svg = d3.select('#map-container').append('svg')
+						.attr('width', settings.width)
+						.attr('height', settings.height)
+						.attr('id', 'full-svg')
+
+			var map_g = d3.select('#full-svg')
+						 .append('g')
+						 .attr('id', 'map-g')
+						 .attr('transform', 'translate ('+settings.padX+',' + settings.padY + ')')
+
+
+//////////////////
+///PLOTTING
+//////////////////
+
+	//assign names to national paths
+		var national_settings = [{'iso3':'GIN',
+								  'color': 'navy'},
+								{'iso3':'LBR',
+								  'color': 'firebrick'},
+								{'iso3':'SLE',
+								  'color': 'olive'}]
+
+
+	// aggregate subnational and national data to get proper colorscales on the district and national level 
+
+		//combine all the subnational data; find the biggest value
+		var all_data = []
+		var summed_vals = {}
+		for (i=0; i<national_settings.length; i++) {
+			var nat_name = national_settings[i]['iso3']
+			var nat_data = all_case_data[nat_name]['Cumulative']
+			var nat_array = d3.keys(nat_data).map(function(d) { return nat_data[d] })
+			all_data.push.apply(all_data, nat_array)
+			summed_vals[nat_name] = d3.sum(nat_array)
+		}
+
+		//set minima and maxima for district-level colorscale
+		var district_min = 0//d3.min(all_data)
+		var district_max = d3.max(all_data)
+
+		//set minima and maxima for national-level colorscale 
+		var summed_array = d3.keys(summed_vals).map(function(d){return summed_vals[d]})
+		var national_min = 0//d3.min(summed_array)
+		var national_max = d3.max(summed_array)
+
+
+	//function for subnat plotting on national click
+		var showSubnats = function (d) {
+
+		//get properties of national value, load shapefile
+			var nat_name = $(this).attr('id')
+			var nat_color = $(this).attr('fill')
+			var subnat_shape = topojson.feature(subnats, subnats.objects[nat_name + '_subnat']).features
+
+		//get rid of the current subnats 
+			map_g.selectAll('.subnational').remove()
+			//$('.subnational').fadeOut(400)
+
+		//load file that maps elements of the ordered list of subnationals (in shapefile) to the subnat names
+			var name_map = subnat_name_mapping[nat_name]
+
+		//load the incidence data
+			var national_cases = all_case_data[nat_name]
+			var cumulative_cases = national_cases['Cumulative']
+
+			// some districts have no reported cases, and as such do not appear in our data.
+			// To ensure that these get mapped appropriately, we add zeroed entries for them 
+			// to our cumulative_cases object.
+			for (j=0; j<d3.keys(name_map).length; j++) {
+				if (d3.keys(cumulative_cases).indexOf(name_map[j])== -1){
+					cumulative_cases[ name_map[j] ] = 0
+				}
+			}
+
+		// set up a color scale using the min and max for districts in all countries, for consistency
+			var color_scale = d3.scale.sqrt().range(['white', nat_color]).domain([district_min, district_max])
+
+		//plot subnats
+			var subnat_paths = map_g.selectAll('.subnational')
+					.data(subnat_shape)
+					.enter()
+					.append('path')
+					.attr('class', 'subnational' )
+					.attr('id', function (d, i) {return name_map[i]})
+					.attr('parent_id', nat_name)
+					.attr('fill', nat_color)
+					.attr('stroke', '#000')
+					.attr('stroke-width', 0.2)
+					.attr('stroke-opacity', 0.5)
+					.attr('d', path)
+
+		//fill in colors
+			subnat_paths.attr('fill', function(d,i){
+				var subnat_name = $(this).attr('id')
+				var value = cumulative_cases[subnat_name]
+				var color = color_scale(value)
+				return color
+			})
+
+		//mouseover text: list name of province and number of cases so far
+			$('#map-g .subnational').poshytip({
+			alignTo: 'cursor', // Align to cursor
+			followCursor: true, // follow cursor when it moves
+			showTimeout: 0, // No fade in
+			hideTimeout: 0,  // No fade out
+			alignX: 'center', // X alignment
+			alignY: 'inner-bottom', // Y alignment
+			className: 'tip-twitter', // Class for styling
+			offsetY: 10, // Offset vertically
+			slide: false, // No slide animation
+			content: function(d){
+				var obj = this.__data__ // Data associated with element
+				var name = $(this).attr('id') // Name from properties
+				var nat_name = $(this).attr('parent_id')
+				var cases = all_case_data[nat_name]['Cumulative'][name] // iso3
+				return name + ' : ' + cases + ' Cases' // String to return
+			}
+			})
+
+			d3.selectAll('.case-hist').remove()
+			var testView = new EbolaView(nat_name)
+	
+	}
+
+
+	//define national data
+	var nat_shape_data = topojson.feature(national, national.objects.layer).features
+
+	//plot outlines of countries
+		var national_paths = map_g.selectAll('.national')
+			.data(nat_shape_data)
+			.enter()
+			.append('path')
+			.attr('class', 'national')
+			.attr('id', function(d,i){return national_settings[i].iso3})
+			.attr('fill', function(d,i){return national_settings[i].color})
+			.attr('fill-opacity', 0.5)
+			.attr('stroke', "#000")
+			.attr('stroke-width', 0.5)
+			.attr('stroke-opacity', 0.5)
+			.attr('d', path)
+		  .on('click', showSubnats)
+
+
+		//make national fill dependent on cumulative cases nationally
+		national_paths.attr('fill', function(d,i){
+			var nat_name = $(this).attr('id')
+			var base_color = national_settings[i]['color']
+			var nat_cases = summed_vals[nat_name]
+			var nat_color_scale = d3.scale.linear().range(['white', base_color]).domain([0, national_max])
+			var nat_color = nat_color_scale(nat_cases)
+			return nat_color
+		})
+
+
+
+
+
+
