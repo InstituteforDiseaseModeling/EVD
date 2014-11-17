b4ec9d5d091435fdd7132e1560ae6132c5f31bef
InstituteforDiseaseModeling <hhu@intven.com>
2014-11-17 15:47:17 -0800

    Revert "Revert "update data through 11/15 (Guinea & SL), 11/13 (Liberia)""
    This reverts commit 08d292c551e530f83e30c52775e21ba498182dc4.

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -5,21 +5,23 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput("country", 
-                         label = h3("Country"), 
-                         choices = list("Sierra Leone" = 1, 
-                                        "Guinea" = 2, "Liberia" = 3),
-                         selected = 1),
+#      checkboxGroupInput("country", 
+#                         label = h3("Country"), 
+#                         choices = list("Sierra Leone" = 1, 
+#                                        "Guinea" = 2, "Liberia" = 3),
+#                         selected = 1),
+      selectInput("country", label="Country", choices = list("Sierra Leone" = 1, 
+                                                             "Guinea" = 2, "Liberia" = 3), selected=1),
       selectInput("aggregation", label = h3("Aggregation"), 
                   choices = list("none"=1, "weekly" = 2, "monthly"=3), selected = 2),
       radioButtons("yscale", label = h3("Scale"),
-                   choices = list("Linear" = 1, "Log" = 2),selected = 2),
-      submitButton("Update")
+                   choices = list("Linear" = 1, "Log" = 2),selected = 2),width =3
     ),
     
     mainPanel(
-      textOutput("text"),
-      plotOutput("plots", width = "100%", height = "600px")
+     # textOutput("text"),
+      plotOutput("plots", inline=T)
+#      plotOutput("plots", width = "100%", height = "600px")
     )
   )
 )
