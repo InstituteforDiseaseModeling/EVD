7833711d3eb25e418ddbda2c6af7aeb55ebee577
Edward Wenger <ewenger@intven.com>
2014-12-29 13:35:33 -0800

    Switch Guinea to WHO public data from December

---
+++ scripts/parse_WHO_data.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pandas as pd
+from datetime import datetime
+
+def format_time_span(epi_week):
+    return datetime.strptime(' '.join(epi_week.split()[-3:]),'%d %B %Y')
+
+def read_case_counts(csv_file,sources=[]):
+    with open(os.path.join('..','data',csv_file),'rb') as f:
+        cases=pd.read_csv(f)
+
+    cases['Epi week']=cases['Epi week'].map(format_time_span)
+
+    # Select sources to compare
+    if sources:
+        cases=cases[cases['Ebola data source'].isin(sources)]
+
+    ## Group including Case definition split
+    #cases.set_index(['Epi week','Location','Ebola data source','Case definition'],inplace=True)
+    #cases_by_district=cases['Numeric'].unstack(['Location','Ebola data source','Case definition'])
+
+    # Sum probable and confirmed
+    cases=cases.groupby(['Epi week','Location','Ebola data source'])['Numeric'].sum()
+    cases_by_district=cases.unstack(['Location','Ebola data source'])
+
+    return cases_by_district
+
+def get_ebola_counts_by_country(countries,sources=['Situation report','Patient database']):
+    cases={}
+    for iso in countries:
+        cases[iso]=read_case_counts('data-text-'+iso+'.csv', 
+                                    sources=sources)
+    return cases
+
+if __name__=='__main__':
+    countries=['GIN']#,'SLE','LBR']
+    cases = get_ebola_counts_by_country(countries,sources=['Situation report'])
+
+    GIN_ebola_districts=['CONAKRY','GUECKEDOU','MACENTA','DABOLA','KISSIDOUGO','DINGUIRAYE','TELIMELE','BOFFA',
+            'KOUROUSSA','SIGUIRI','PITA',"N'ZEREKORE",'YOMOU','DUBREKA','FORECARIAH','KEROUANE','COYAH',
+            'DALABA','BEYLA','KINDIA','LOLA','BOKE','FARANAH','KANKAN']
+
+    date_after=datetime(2014,12,1)
+
+    GIN_sitrep_cases=cases['GIN'].ix[date_after:].swaplevel(0,1,axis=1)['Situation report'][GIN_ebola_districts]
+
+    print(GIN_sitrep_cases)
+
+    GIN_sitrep_cases.to_csv('tmp_GIN_cases.csv')
